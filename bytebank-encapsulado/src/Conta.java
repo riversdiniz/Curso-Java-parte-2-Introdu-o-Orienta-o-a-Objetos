999c7a46dbffdff4f241b14f8dedc84b1cc1005a
River Diniz <river-s-diniz@hotmail.com>
2022-08-16 12:12:29 -0300

    Add get e set linha 49 e 51.

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -41,4 +41,12 @@ public class Conta {
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
 }
