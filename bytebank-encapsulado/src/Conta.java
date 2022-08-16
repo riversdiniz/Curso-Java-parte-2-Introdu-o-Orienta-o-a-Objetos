9b552e7c5efd7b39ce5001a74a38100c8cdf17ac
River Diniz <river-s-diniz@hotmail.com>
2022-08-16 12:09:38 -0300

    Alterando o nome do atributo.

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -38,7 +38,7 @@ public class Conta {
         return this.numero;
     }
 
-    public void setNumero(int novoNumero) {
-        this.numero = novoNumero;
+    public void setNumero(int numero) {
+        this.numero = numero;
     }
 }
