3c4a00dc4de62c0f6967a1b2a5ce21f456e324fc
River Diniz <river-s-diniz@hotmail.com>
2022-08-17 15:37:30 -0300

    Adicionando mais duas conta em TestaValores.

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -8,7 +8,7 @@ public class Conta {
 
     public Conta(int agencia, int numero){
         total ++;
-        System.out.println("o total de contas é" + total);
+        System.out.println("o total de contas é " + total);
         this.agencia = agencia;
         this.numero = numero;
         System.out.println("estou criando uma conta " + this.numero);
